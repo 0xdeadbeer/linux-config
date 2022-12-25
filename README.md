f2f7774c7444474719adcbbacc245d7ac6a5b125
osamu-kj <kevin.jerebica@gmail.com>
2022-12-25 11:35:10 +0100

    Config: Update i3 and add config for neovim

--- README.md
+++ README.md
@@ -5,8 +5,10 @@ Configuration files & scripts I usually use for my linux rice & work-station.
 This repository may include anything related to my style of ricing Linux. 
 
 Files: 
-  - `/config/i3-config.conf` goes into `~/.config/i3/config`
-  - `/config/i3-bar-config.conf` goes into `/etc/i3status.conf`
-  - `/config/vim-config.conf` goes into `~/.vimrc`
-  - `/scripts/backup-manager.sh` is a script for backing up personal files, run it without arguments to get the help page 
-  - `/scripts/start-i3.sh` is a script i use to start I3 on boot alongside KDE
+ - `/config/i3-config.conf` goes into `~/.config/i3/config`
+ - `/config/i3-bar-config.conf` goes into `/etc/i3status.conf`
+ - `/config/vim-config.conf` goes into `~/.vimrc`
+ - `/config/nvim/init.vim` goes into `~/.config/nvim/init.vim`
+ - `/config/nvim/plugins.lua` goes into `/.config/nvim/lua/plugins.lua`
+ - `/scripts/backup-manager.sh` is a script for backing up personal files, run it without arguments to get the help page 
+ - `/scripts/start-i3.sh` is a script i use to start I3 on boot alongside KDE
---
+++ config/neovim/plugins.lua
@@ -0,0 +1,82 @@
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Simple plugins can be specified as strings
+  use 'rstacruz/vim-closer'
+
+  -- Lazy loading:
+  -- Load on specific commands
+  use {'tpope/vim-dispatch', opt = true, cmd = {'Dispatch', 'Make', 'Focus', 'Start'}}
+
+  -- Load on an autocommand event
+  use {'andymass/vim-matchup', event = 'VimEnter'}
+
+  -- Load on a combination of conditions: specific filetypes or commands
+  -- Also run code after load (see the "config" key)
+  use {
+    'w0rp/ale',
+    ft = {'sh', 'zsh', 'bash', 'c', 'cpp', 'cmake', 'html', 'markdown', 'racket', 'vim', 'tex'},
+    cmd = 'ALEEnable',
+    config = 'vim.cmd[[ALEEnable]]'
+  }
+
+  -- Plugins can have dependencies on other plugins
+  use {
+    'haorenW1025/completion-nvim',
+    opt = true,
+    requires = {{'hrsh7th/vim-vsnip', opt = true}, {'hrsh7th/vim-vsnip-integ', opt = true}}
+  }
+
+  -- Plugins can also depend on rocks from luarocks.org:
+  use {
+    'my/supercoolplugin',
+    rocks = {'lpeg', {'lua-cjson', version = '2.1.0'}}
+  }
+
+  -- You can specify rocks in isolation
+  use_rocks 'penlight'
+  use_rocks {'lua-resty-http', 'lpeg'}
+
+  -- Local plugins can be included
+  use '~/projects/personal/hover.nvim'
+
+  -- Plugins can have post-install/update hooks
+  use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'}
+
+  -- Post-install/update hook with neovim command
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
+  -- Post-install/update hook with call of vimscript function with argument
+  use { 'glacambre/firenvim', run = function() vim.fn['firenvim#install'](0) end }
+
+  -- Use dependency and run lua function after load
+  use {
+    'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
+    config = function() require('gitsigns').setup() end
+  }
+
+  -- You can specify multiple plugins in a single call
+  use {'tjdevries/colorbuddy.vim' }
+
+  -- You can alias plugin names
+  use {'dracula/vim', as = 'dracula'}
+
+  use {'chriskempson/base16-vim', as = 'base16-vim'}
+
+  use {
+    'nvim-telescope/telescope.nvim', tag='0.1.0',
+
+    requires = {{ 'nvim-lua/plenary.nvim' }}
+  }
+
+  use {
+    "nvim-lualine/lualine.nvim",
+    requires = {"kyazdani42/nvim-web-devicons", opt = true}
+  }
+
+  use { "gko/vim-coloresque" }
+
+  use { "ellisonleao/gruvbox.nvim" }
+
+end)
